4646b2b6467c9c399c527e2d5f34361d386f91a1
IntroPret <alfred.dexter.2005@gmail.com>
2025-10-01 21:03:52 +0700

    Adding and Fixing Projects Card

--- src/data/project.js
+++ src/data/project.js
@@ -17,7 +17,7 @@ export const allProjects = [
         id: 3,
         title: 'AiSee | IoT Smart Classroom System',
         description: 'An IoT + AI camera prototype that detects cheating behavior and automates student attendance, with real-time monitoring via Ubidots dashboard and MongoDB integration.',
-        imageUrl: '/iot-classroom.jpg',
+        imageUrl: '/aisee-prototype.jpg',
         tags: ['Streamlit', 'MicroPython', 'Ubidots', 'OpenCV', 'Firebase', 'IoT'],
       },
       {
@@ -26,19 +26,5 @@ export const allProjects = [
         description: 'A research project comparing transfer learning models (VGG19, MobileNetV2, YOLOv8) for Indonesian sign language classification. Dataset preprocessing, model training, and evaluation included.',
         imageUrl: '/bisindo.jpg',
         tags: ['Deep Learning', 'Transfer Learning','TensorFlow', 'YOLOv8', 'Python'],
-      },
-      {
-        id: 5,
-        title: 'Project Five',
-        description: 'A brief description of the fifth amazing project that showcases your skills in React and modern web development.',
-        imageUrl: 'https://placehold.co/600x400/34d399/1f2937?text=Project+5',
-        tags: ['React', 'Node.js', 'CSS Grid'],
-      },
-      {
-        id: 6,
-        title: 'Project Six',
-        description: 'A brief description of the sixth amazing project, highlighting your ability to solve complex problems.',
-        imageUrl: 'https://placehold.co/600x400/34d399/1f2937?text=Project+6',
-        tags: ['JavaScript', 'API', 'SCSS'],
       },
 ];
